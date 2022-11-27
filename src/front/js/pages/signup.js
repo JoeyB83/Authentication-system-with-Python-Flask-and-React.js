469c5a47a562c8d41b90b8a705673025f617d18e
JoeyB83 <joealebarranco@gmail.com>
2022-11-27 20:13:32 +0000

    My first commit

---
+++ src/front/js/pages/signup.js
@@ -0,0 +1,68 @@
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+export const SignUp = () => {
+  const { store, actions } = useContext(Context);
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userActive, setUserActive] = useState(false);
+  const navigate = useNavigate();
+
+  console.log(userActive);
+
+  return (
+    <div className="container">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={(e) => setUserEmail(e.target.value)}
+            value={userEmail}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={(e) => setUserPassword(e.target.value)}
+            value={userPassword}
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+            onChange={(e) => setUserActive(e.target.checked)}
+            value={userActive}
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Active
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary" 
+          onClick={(e) => {
+              e.preventDefault();
+              navigate("/");
+            }}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,7 +1,8 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
+			token: null,
+			verifieUser: false,
 			demo: [
 				{
 					title: "FIRST",
@@ -17,36 +18,112 @@ const getState = ({ getStore, getActions, setStore }) => {
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getToken: (email, password) => {
+				fetch(process.env.BACKEND_URL + '/api/token',{
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						email: email,
+						password: password
+					}),
+					redirect: "follow"
+				})
+				.then(response => response.json())
+				.then(result => {setStore({token: result.token})})
+				.catch(err => console.log(err));
 			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getVerified: () => {
+				fetch(process.env.BACKEND_URL + '/api/protected',{
+					method: 'GET',
+						headers: {
+							"Authorization": "Bearer " + getStore().token
+						},						
+						redirect: "follow"
+				})
+				.then((res) => res.ok ? setStore({verifieUser: true}):"")
+				.catch((err) => console.log(err));
 			}
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
+
+			// syncTokenFromSessionStore: () => {
+			// 	const token = sessionStorage.getItem("token");
+			// 	console.log("Aplication just loaded, synching the session storage token")
+			// 	if (token && token!="" && token != undefined) setStore({ token: token});
+			// },
+
+			// logout: () => {
+			// 	sessionStorage.removeItem("token");
+			// 	console.log("Login out")
+			// 	setStore({ token: null});
+			// },
+
+			// login: async (email, password) => {
+			// 	const opts = {
+			// 		method: "POST",
+			// 		headers: {
+			// 			"Content-Type": "application/json"
+			// 		},
+			// 		body: JSON.stringify({
+			// 			"email": email,
+			// 			"password": password
+			// 		})
+			// 	};
+
+			// 	try{
+			// 		const resp = await fetch("https://3001-4geeksacade-reactflaskh-8i7x41mrwnz.ws-us77.gitpod.io/api/token", opts)
+			// 		if(resp.status !== 200){
+			// 			alert("There has been some error");
+			// 			return false;
+			// 		}
+					
+			// 		const data = await resp.json();
+			// 		console.log("this came from the backend", data)
+			// 		sessionStorage.setItem("token", data.access_token)
+			// 		setStore({ token: data.access_token})
+			// 		return true;					
+			// 	}
+			// 	catch(error){
+			// 		console.error("There has been an error login in")
+			// 	}
+			// },
+
+			// getMessage: async () => {
+			// 	const store = getStore();
+			// 	const opts = {
+			// 		headers: {
+			// 			"Authorization": "Bearer " + store.token
+			// 		}
+			// 	}
+			// 	try{
+			// 		// fetching data from the backend
+			// 		const resp = await fetch("https://3001-4geeksacade-reactflaskh-8i7x41mrwnz.ws-us77.gitpod.io/api/hello", opts)
+			// 		const data = await resp.json()
+			// 		setStore({ message: data.message })
+			// 		// don't forget to return something, that is how the async resolves
+			// 		return data;
+			// 	}catch(error){
+			// 		console.log("Error loading message from backend", error)
+			// 	}
+			// },
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
+
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
 		}
 	};
 };
